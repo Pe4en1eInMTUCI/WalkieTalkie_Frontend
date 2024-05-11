428107395f41f99c70c379094beabf63e9ddabd3
Pe4en1e <62511045+Pe4en1e@users.noreply.github.com>
2024-05-11 05:55:41 +0300

    login

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
-from flask import Flask, render_template
+import json
+
+from flask import Flask, render_template, redirect, request, session, url_for
 import requests
 
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/login")
-def root():
-    return render_template('login.html')
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        phone = request.form.get("phone")
+        password = request.form.get("password")
+
+        data = {
+            "phone": phone,
+            "password": password
+        }
+
+        req = requests.post("http://127.0.0.1:1232/api/login", data=data).json()['status']
+
+        match req:
+            case "OK":
+                return redirect(url_for('/chats', phone=phone))
+            case "wrong password":
+                pass
+            case "user not exists":
+                pass
+
+
+        return render_template('login.html')
+    if request.method == "GET":
+        return render_template('login.html')
 
 @app.route("/register")
 def reg():
@@ -22,16 +46,19 @@ def smscon():
     return render_template('sms-confirm.html')
 
 
-@app.route("/chats")
+@app.route("/chats", methods=['POST'])
 def chats():
 
+    phone = request.form.get("phone")
+
     data = {
-        "username": "pe4en1e"
+        "phone": phone
     }
 
     req = requests.post("http://127.0.0.1:1232/api/getdialogs", data=data).json()
 
     return render_template('chats.html', chatlist=req)
+
 
 
 if __name__ == "__main__":
