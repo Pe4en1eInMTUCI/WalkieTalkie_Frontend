4159fe05b9cbbbde77779b389d4d67888660f0b0
Pe4en1e <62511045+Pe4en1e@users.noreply.github.com>
2024-05-11 20:29:35 +0300

    phone-confirmation

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from flask import Flask, render_template, redirect, request, session, url_for
 import requests
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8b8e1c38fabc194db7de522c7d736b9e79a9badb'
 
 @app.route("/")
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == "POST":
+
         phone = request.form.get("phone")
         password = request.form.get("password")
 
@@ -17,9 +19,9 @@ def login():
             "password": password
         }
 
+
         req = requests.post("http://127.0.0.1:1232/api/login", data=data).json()['status']
 
-        print(req)
 
         match req:
             case "OK":
@@ -29,8 +31,6 @@ def login():
             case "user not exists":
                 return render_template('login.html', errorMessage = 'Такого пользователя не существует!')
 
-
-        return render_template('login.html')
     if request.method == "GET":
         return render_template('login.html')
 
@@ -62,20 +62,62 @@ def reg():
 
     match req:
         case "OK":
-            pass
+
+            userdata = {
+                "username": username,
+                "phone": phone,
+                "password": password
+            }
+
+            session['userdata'] = userdata
+
+            return redirect("/phone-confirm")
         case "username exists":
             return render_template('register.html', errorMessage="Имя пользователя занято!")
         case "phone exists":
             return render_template('register.html', errorMessage="Номер телефона занят!")
 
 
-@app.route('/phoneconfirm')
+@app.route('/phone-confirm', methods=["POST", "GET"])
 def phcon():
-    return render_template('phone-confirm.html')
+    if request.method == "GET":
 
-@app.route("/smsconfirm")
-def smscon():
-    return render_template('sms-confirm.html')
+        if not 'userdata' in session:
+            return render_template('login.html', errorMessage="INVALID SESSION!")
+
+        userIP = -1
+        code = requests.get(f"http://127.0.0.1:1232/api/voicecode?userIP={userIP}&userPhone=str({session['userdata']['phone']})").json()['code']
+
+
+        session['voicecode'] = code
+
+
+        return render_template('phone-confirm.html', userdata=session['userdata'])
+
+
+    inputCode = request.form.get("code")
+
+
+    if inputCode == session['voicecode']:
+
+        data = {
+            "phone": session['userdata']['phone'],
+            "username": session['userdata']['username'],
+            "password": session['userdata']['password']
+        }
+
+        reg_status = requests.post("http://127.0.0.1:1232/api/register", data=data).json()['status']
+
+        if reg_status == "OK":
+            return redirect(url_for('chats'), code=307)
+
+
+    return render_template('phone-confirm.html', errorMessage="Неверный код!", userdata=session['userdata'])
+
+
+# @app.route("/sms-сonfirm")
+# def smscon():
+#     return render_template('sms-confirm.html')
 
 
 @app.route("/chats", methods=['POST', 'GET'])
@@ -85,14 +127,17 @@ def chats():
 
 
     phone = request.form.get('phone')
+    if phone == None:
+        phone = session['userdata']['phone']
+
+    session.clear()
 
     data = {
         "phone": phone
     }
 
-    username = requests.post("http://127.0.0.1:1232/api/getUsernameByPhone", data={"phone": phone}).json()
 
-    print(username)
+    username = requests.post("http://127.0.0.1:1232/api/getUsernameByPhone", data={"phone": phone}).json()
 
     req = requests.post("http://127.0.0.1:1232/api/getdialogs", data=data).json()
 
